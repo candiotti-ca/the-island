acf113b05d38203d241ddc381454aad8d7d27953
Carla <Carla.candiotti@hotmail.fr>
2023-11-27 10:45:06 +0100

    WIP draw hexagons

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,31 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="hexagon"></div>
+
+<style>
+	.hexagon {
+		margin: 40px;
+		position: relative;
+		width: 92px; /* 46% de 200 */
+		height: 160px; /*80% de 200 */
+		background-color: #fda3b2;
+		cursor: pointer;
+		border: 3px solid black;
+	}
+
+	.hexagon:before,
+	.hexagon:after {
+		position: absolute;
+		content: '';
+		background: inherit;
+		height: 100%;
+		width: 100%;
+		transform-origin: center;
+		border-top: 3px solid black;
+		border-bottom: 3px solid black;
+	}
+	.hexagon:before {
+		transform: rotateZ(60deg);
+	}
+	.hexagon:after {
+		transform: rotateZ(-60deg);
+	}
+</style>
